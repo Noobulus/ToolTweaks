ebb033516bb5682844cd6583ca885d3cfef1eebe
Noobulus <mattmattiasmatthew@gmail.com>
2022-09-19 18:59:42 -0500

    setup metadata and make sure the darn thing gets to the title screen

--- src/main/java/mod/noobulus/tooltweaks/mixin/TitleScreenMixin.java
+++ src/main/java/mod/noobulus/tooltweaks/mixin/TitleScreenMixin.java
@@ -1,6 +1,6 @@
-package com.example.example_mod.mixin;
+package mod.noobulus.tooltweaks.mixin;
 
-import com.example.example_mod.ExampleMod;
+import mod.noobulus.tooltweaks.ToolTweaks;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
-	public void exampleMod$onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+	public void tooltweaks$onInit(CallbackInfo ci) {
+		ToolTweaks.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
